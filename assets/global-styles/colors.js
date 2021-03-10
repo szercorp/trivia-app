b6038046e0099b3d53cfdd412ee088cbe2eff1a7
szercorp <szer.corp@gmail.com>
2021-03-10 17:47:12 +0200

    Some changes

--- assets/global-styles/colors.js
+++ assets/global-styles/colors.js
@@ -1,25 +1,9 @@
 export const colors = {
-  pink: '#E03B93',
-  purple: '#8E4491',
-  blue: '#20498C',
-  darkGray: '#373535',
-  disabledGray: '#858484',
-  white: '#fafafa',
-  offWhite: '#f7f7f7',
-  genres : {
-    alternative: '#90A564',
-    blues: '#4D70A9',
-    country: '#BC9578',
-    dance: '#87CCD3',
-    electronic: '#EAC37C',
-    jazz: '#8793DE',
-    latin: '#DC9D6D',
-    pop: '#6AB4B1',
-    rnb: '#DF7770',
-    reggae: '#7FC38A',
-    rock: '#5A90CE',
-    world: '#C89BEB',
-    hiphop: '#DF739D',
-
-  }
+  pink: "#E03B93",
+  purple: "#8E4491",
+  blue: "#20498C",
+  darkGray: "#373535",
+  disabledGray: "#858484",
+  white: "#fafafa",
+  offWhite: "#f7f7f7",
 };
